f554468eec3ae47fe9be64f74c3cb8b858b98c17
Robb Marsee <robb.marsee@gamil.com>
2016-08-24 11:36:07 -0700

    remove amazon s3 keys

--- app/config/AMAZON_S3_KEYS.js
+++
@@ -1,8 +0,0 @@
-const AMAZON_S3 = {
-  bucket: 'nearbyapp',
-  region: 'us-west-1',
-  accessKey: 'AKIAJPF5NM7E5V4OLPDQ',
-  secretKey: '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'
-};
-
-export default AMAZON_S3;
