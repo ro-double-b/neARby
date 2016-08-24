86d470c77cba1b06197082a505ccff9addc741ae
Robb Marsee <robb.marsee@gamil.com>
2016-08-24 10:52:04 -0700

    commit all files before rebase

---
+++ app/config/AMAZON_S3_KEYS.js
@@ -0,0 +1,8 @@
+const AMAZON_S3 = {
+  bucket: 'nearbyapp',
+  region: 'us-west-1',
+  accessKey: 'AKIAJPF5NM7E5V4OLPDQ',
+  secretKey: '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'
+};
+
+export default AMAZON_S3;
